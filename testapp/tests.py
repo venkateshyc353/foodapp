920197bfb6619f8e4a347e797cd997f1eca79ded
venkatesh <venkateshyc353@gmail.com>
2019-08-08 21:39:22 +0530

    second commit

---
+++ testapp/tests.py
@@ -0,0 +1,9 @@
+# a='my name si venkat india'
+# b={}.fromkeys(a,0)
+#
+# for h in range(len(a)):
+#     b[a[h]]=b[a[h]]+1
+# dd=sorted(b.items(),key=lambda a : a[1])
+# print(dict(dd))
+aa=[[4,5],[7,8]]
+print(dict(aa))
